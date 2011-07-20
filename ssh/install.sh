a4d66ea6a6d34d0b3a33ff9a2743f77180c49fba
Kale Kundert <kale@theKunderts.net>
2011-07-20 01:34:17 -0700

    Fix a bug in the ssh installation script.

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env zsh
 
+echo "Configuring ssh."
+echo $ROOT >&2
+
 # Make sure the ~/.ssh directory exists.
 mkdir -p ~/.ssh
+
+# Move into the ssh configuration directory.
+cd ssh
 
 # Try to link each configuration file into the ~/.ssh directory.  If any of
 # these links cannot be created, give up and report failure.
 for FILE in *; do
-    ln -s $ROOT/$FILE ~/.ssh
+
+    rm -f ~/.ssh/$FILE
+    ln -s $ROOT/ssh/$FILE ~/.ssh
+
     [ $? -ne 0 ] && exit 1
+
 done
 
 # Return a success code if nothing failed.
