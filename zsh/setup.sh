c66dd416426eac3764304979a5abc63cf6b7b29b
Kale Kundert <kale@theKunderts.net>
2011-07-20 23:26:13 -0700

    Add support for custom command-line completion.

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -1,11 +1,17 @@
-
+# Source all of the configuration files.
 ENV_VARS_PATH=~/.zsh/env-vars.sh
 ALIASES_PATH=~/.zsh/aliases.sh
 PROMPT_PATH=~/.zsh/prompt.sh
 
+COMPLETION_PATH=~/.zsh/completion
+
 test -s $ENV_VARS_PATH && source $ENV_VARS_PATH
 test -s $ALIASES_PATH && source $ALIASES_PATH
 test -s $PROMPT_PATH && source $PROMPT_PATH 
+
+for COMPLETION in $COMPLETION_PATH/*.sh; do
+    source $COMPLETION
+done
 
 # Configure command-line history.
 HISTFILE=~/.zhistory
@@ -20,7 +26,5 @@ setopt extended_glob
 
 # The following lines were added by compinstall
 zstyle :compinstall filename '/home/kale/.zshrc'
-
 autoload -Uz compinit && compinit
-# End of lines added by compinstall
 
